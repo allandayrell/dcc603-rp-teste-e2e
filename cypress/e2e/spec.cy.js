290cd03e56854984fea81fcedb809a83bf429b85
Allan Dayrell <allan.dayrell@gmail.com>
2025-06-15 22:18:44 -0300

    Adição de 3 novos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,16 +4,16 @@ describe('TODOMvc App', () => {
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de Engenharia de Software{enter}');
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 1) 
+      .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de Engenharia de Software'); 
+      .should('have.text', 'TP2 de Engenharia de Software');
   });
 
   it('Insere e deleta uma tarefa', () => {
@@ -36,7 +36,7 @@ describe('TODOMvc App', () => {
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de ES{enter}')
@@ -67,5 +67,51 @@ describe('TODOMvc App', () => {
     cy.get('[data-cy=todos-list]')
       .children()
       .should('have.length', 2);
+  });
+
+  it('Deve marcar todas as tarefas como concluídas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+    .type('Acordar o filho{enter}')
+    .type('Dar café{enter}')
+    .type('Levar para a escola{enter}');
+
+    cy.get('.toggle-all-label').click();
+    cy.get('[data-cy=todos-list] li').each(($el) => {
+      cy.wrap($el).should('have.class', 'completed');
+    });
+  });
+
+  it('Deve editar uma tarefa existente', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]').type('Tarefa original{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+      .dblclick();
+
+    cy.get('[data-cy=todos-list] > li.editing .edit')
+      .clear()
+      .type('Tarefa editada com sucesso{enter}');
+
+    cy.get('[data-cy=todos-list] > li').should('have.text', 'Tarefa editada com sucesso');
+    cy.get('[data-cy=todos-list]').children().should('have.length', 1);
+  });
+
+  it('Deve limpar todas as tarefas completadas', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]').type('Fazer bolo{enter}')
+      .type('Comprar ovos{enter}');
+
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]').children().should('have.length', 1)
+      .first()
+      .should('have.text', 'Comprar ovos');
   });
 });
